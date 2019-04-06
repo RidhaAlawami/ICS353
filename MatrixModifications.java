78a3734739558c30b898acd86b83e035e8040f94
Ridha Alawami <xtok96@gmail.com>
2019-04-06 14:52:08 +0300

    Add files via upload

--- MatrixModifications.java
+++ MatrixModifications.java
@@ -1,51 +1,87 @@
 
 public class MatrixModifications {
-	
-	
 
-	//This methods adds 1 to the array row and column.
-	//and returns a new array
-	//assuming its a matrix.
-	public  int[][] AddPadding(int[][] array){
-			int[][] my_padded_array = null;
-			if(array == null || array.length == 0)
-				return array;
-			
-			else {
-				int array_size = array.length + 1;
-				
-				my_padded_array = new int[array_size][array_size];
-				
-				for(int i = 0; i < array.length; i++) {
-					for(int j = 0; j < array[i].length; j++) {
-						my_padded_array[i][j] = array[i][j];
-					}
-				}	
-			}
-			
-			return my_padded_array;
-		}
-		
-	//This methods removes 1 from the array row and column.
-	//and returns a new array
-	//assuming its a matrix.
-	public  int[][] RemovePadding(int[][] array){
-			int[][] my_unpadded_array = null;
-			if(array == null || array.length == 0)
-				return array;
-			
-			else {
-				int array_size = array.length - 1;
-				
-				my_unpadded_array = new int[array_size][array_size];
-				for(int i = 0; i < array_size; i++) {
-					for(int j = 0; j < array_size; j++) {
-						my_unpadded_array[i][j] = array[i][j];
-					}
-				}	
-			}
-			return my_unpadded_array;
-		}
-		
+	private static int matrix_size_before_padding;
 
+	// This methods adds a padding factor using GetPaddingFactor to the matrix row
+	// and column.
+	// and returns a new matrix
+	// assuming the input is a matrix.
+	public int[][] AddPadding(int[][] matrix) {
+		int[][] my_padded_matrix = null;
+		if (matrix == null || matrix.length == 0)
+			return matrix;
+
+		else {
+			// to be used in RemovePadding
+			matrix_size_before_padding = matrix.length;
+			int matrix_size = NextPowerOfTwo(matrix.length);
+
+			my_padded_matrix = new int[matrix_size][matrix_size];
+
+			for (int i = 0; i < matrix.length; i++) {
+				for (int j = 0; j < matrix[i].length; j++) {
+					my_padded_matrix[i][j] = matrix[i][j];
+				}
+			}
+		}
+
+		return my_padded_matrix;
+	}
+
+	// This methods removes the previously added padding factor from the matrix row
+	// and column.
+	// and returns a new matrix
+	// assuming the input is a matrix.
+	public int[][] RemovePadding(int[][] matrix) {
+		int[][] my_unpadded_matrix = null;
+		if (matrix == null || matrix.length == 0)
+			return matrix;
+
+		else {
+			int matrix_size = matrix_size_before_padding;
+
+			my_unpadded_matrix = new int[matrix_size][matrix_size];
+			for (int i = 0; i < matrix_size; i++) {
+				for (int j = 0; j < matrix_size; j++) {
+					my_unpadded_matrix[i][j] = matrix[i][j];
+				}
+			}
+		}
+		return my_unpadded_matrix;
+	}
+
+	// closet 2^x number to a given input
+	private static int NextPowerOfTwo(int n) {
+
+		int closestNumber = 0;
+
+		double log2n = Math.log(n) / Math.log(2);
+
+		double power = Math.ceil(log2n);
+
+		closestNumber = (int) Math.pow(2, power);
+
+		return closestNumber;
+	}
+
+	// this takes the n of the matrix and calculates the factor based on the minimum
+	// between the n and a 2 to power.
+	// to make the padding in a power of two.
+//	public int GetPaddingFactor(int n) {
+//		int padding_factor = 0;
+//		int temp = 2, counter = 1;
+//		boolean factor_found = false;
+//		
+//		while(!factor_found) {
+//			if(n-temp >0) {
+//				temp = (int) Math.pow(2, ++counter);
+//			}
+//			else {
+//				padding_factor = temp - n;
+//				factor_found = true;
+//			}
+//		}
+//		return padding_factor;
+//	}
 }
--- driver.java
+++ driver.java
@@ -1,10 +1,115 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// n*n
+				int n;	
 
-	}
+				//matrix multiplication from file
+				String input_file_name = "";
+				Scanner kb = new Scanner(System.in);
+
+				System.out.println("Input format: n *ENTER* InputFileName.txt");
+				System.out.println("For example: 1011 \ninput_file_name.txt");
+
+				n = kb.nextInt();
+				input_file_name = kb.next();
+				
+				
+				//get matrix A and matrix B
+				Matrices readMatrcies = matrix(n, input_file_name);
+
+				readMatrcies.printBothArrays();
+				
+				MatrixMultiplication inputMatrices = new MatrixMultiplication(readMatrcies.getMatrixA(), readMatrcies.getMatrixB());
+				int[][] iterativeResults = inputMatrices.IterativeMultiplication(readMatrcies.getMatrixA(), readMatrcies.getMatrixB(), n);
+				
+				for(int i = 0 ; i < iterativeResults.length; i ++) {
+					for(int j = 0; j < iterativeResults[i].length; j++) {
+						System.out.print(iterativeResults[i][j] + "\t");
+					}
+					System.out.println();
+				}
+				
+			}
+
+			
+			//get the matrces from the file 
+			public static Matrices matrix(int n, String input_file_name){
+				int[][] matrixA = new int[n][n];
+				int[][] matrixB = new int[n][n];
+
+				try {
+					Scanner inputFile = new Scanner(new File(input_file_name));
+					int i = 0;
+					int counter = 0;
+					boolean middle = false;
+					while(counter < n*2) {
+							if(counter < n) {
+								for(int j = 0; j < n; j++) {
+									matrixA[i][j] = inputFile.nextInt();
+								}
+								
+								i++;
+								counter++;
+							}
+
+							else if(counter >= n) {
+								if(!middle) {
+									i = 0;
+									middle = true;
+								}
+								for(int j = 0; j < n; j++) {
+									matrixB[i][j] = inputFile.nextInt();
+								}
+								i++;
+								counter++;
+							}
+					}
+
+					inputFile.close();
+
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Could not find a file named " + input_file_name);
+				}
+				
+				
+				return new Matrices(matrixA, matrixB);
+			}
+
+			
+			//read the result to file
+			public static void writeToFile(String fileName, int matrixC [][],  int n) throws IOException{
+				
+				BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+				writer.write("iterative matrix multiplication result of "+n +"*" +  ":\n");
+				writer.newLine();
+				
+				//print the matrixC as 
+				/*
+				 * 2 * 2 matrix 
+				 * 1   2
+				 * 4   5
+				 * 
+				 */
+				for(int row = 0; row < n; row++){
+					for(int column = 0; column < n; column++){
+						writer.write(matrixC[row][column] + " ");
+					}
+					writer.newLine();
+				}
+				
+				
+				writer.close();	
+			}
 	
 
 	
